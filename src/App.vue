9f3025eda387e2ef4a4b77cc4aed49f817867d2d
ashley revlett <ashleyrevlett@gmail.com>
2022-11-14 15:44:19 -0500

    Add intro view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Intro from './views/Intro.vue'
 import Game from './views/Game.vue'
+
+export default defineComponent({
+  components: {
+    Intro,
+    Game
+  },
+  data() {
+    return {
+      showIntro: true,
+    }
+  }
+})
 </script>
 
 <template>
-  <Game />
+  <Intro v-if="showIntro" @start="showIntro = false" />
+  <Game v-if="showIntro == false" />
 </template>
 
 <style scoped>
---
+++ src/views/Intro.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { SPICE_ORDER } from '../types'
+import { LocationSpice } from '../models/LocationSpice'
+
+export default defineComponent({
+  methods: {
+    getSpices() {
+      const spices : Array<LocationSpice> = Array();
+      SPICE_ORDER.forEach(spice => {
+        spices.push(new LocationSpice(spice))
+      });
+      return spices
+    },
+  }
+})
+</script>
+
+<template>
+
+  <main class="text-center">
+    <p>This is a game of buying and selling. The object of the game is to pay off your debt to the bank. Then, make as much money as you can in a 1 year period. The prices of spices per unit are:</p>
+    <ul>
+      <li v-for="spice in getSpices()">{{ spice.spiceType }}: ${{spice.priceRange.min}} – {{spice.priceRange.max}} </li>
+    </ul>
+    <button @click="$emit('start')">Start Game</button>
+  </main>
+
+
+</template>
+
+
+<style scoped>
+
+  main {
+    max-width: 80%;
+    margin: 0 auto;
+  }
+
+  ul {
+    padding: 0;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+</style>
